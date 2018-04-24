9048c4e4b0f0a5d87765669637ad39788d5dba6f
saharsamr <sahar.rajabi76@gmail.com>
2018-04-24 18:07:56 +0430

    adding data to houseList function added.

--- src/main/java/DataManager/HouseListDataHandler.java
+++ src/main/java/DataManager/HouseListDataHandler.java
@@ -21,9 +21,12 @@ public class HouseListDataHandler {
         public void run(){
             try {
                 JSONObject res = JsonParser.getJSONResponse("http://acm.ut.ac.ir/khaneBeDoosh/house");
-                Timestamp  expireTime = new Timestamp(res.getLong("expireTime")),
-                        timestamp = new Timestamp(System.currentTimeMillis());
+                Timestamp timestamp = new Timestamp(System.currentTimeMillis()),
+                        expireTime = new Timestamp(res.getLong("expireTime"));
                 delay = expireTime.getTime() - timestamp.getTime();
+                String sqlCommand = "DELETE FROM estatesList";
+                DataBaseHandler.executeStatement(sqlCommand);
+                addToDatabase(res.getJSONArray("data"));
             }catch (Exception e){
                 System.out.println(e.getMessage());
             }
@@ -33,18 +36,29 @@ public class HouseListDataHandler {
     public static void addToDatabase(JSONArray estatesList){
         for(int i = 0; i < estatesList.length(); i++){
             ArrayList<String> values = getObjectData(estatesList.getJSONObject(i));
+            addToList(values);
         }
     }
 
-    private static ArrayList<String> getObjectData(JSONObject obj){
+    private static ArrayList<String> getObjectData(JSONObject obj){ //TODO: use map instead of simple array.
         ArrayList<String> values = new ArrayList<String>();
         values.add(obj.get("id").toString());
         values.add(obj.get("buildingType").toString());
-        values.add(obj.get("dealType").toString());
+        String dealType = obj.get("dealType").toString();
+        values.add(dealType);
         values.add(obj.get("area").toString());
         values.add(obj.get("imageURL").toString());
-        values.add(obj.get("price").toString());
-
+        JSONObject price = new JSONObject(obj.get("price").toString());
+        if(dealType.equals("0")){
+            values.add(price.get("sellPrice").toString());
+            values.add("0");
+            values.add("0");
+        }
+        else{
+            values.add(price.get("0").toString());
+            values.add(price.get("basePrice").toString());
+            values.add(price.get("rentPrice").toString());
+        }
         return values;
     }
 
@@ -60,5 +74,23 @@ public class HouseListDataHandler {
                     "rentPrice INTEGER" +
                     ");";
             DataBaseHandler.executeStatement(sqlCommand);
+    }
+
+    public static void addToList(ArrayList<String> values){
+        ArrayList<String> attrs = makeEstateAttributeList();
+        DataBaseHandler.addItem("estatesList", attrs, values);
+    }
+
+    private static ArrayList<String> makeEstateAttributeList(){
+        ArrayList<String> attrs = new ArrayList<String>();
+        attrs.add("id");
+        attrs.add("buildingType");
+        attrs.add("dealType");
+        attrs.add("area");
+        attrs.add("imageURL");
+        attrs.add("sellPrice");
+        attrs.add("basePrice");
+        attrs.add("rentPrice");
+        return attrs;
     }
 }
