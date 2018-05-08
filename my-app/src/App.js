4774d64b2398e9b7ec20027792f3a02a181dd154
SadafNiknam <sadaf.niknaam@gmail.com>
2018-05-08 14:05:23 +0430

    login page added.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,7 @@ import IncreaseCredit from "./Credit/IncreaseCredit";
 import HouseDetailPage from "./HouseDirectory/HouseDetailPage";
 import AddHouses from "./AddHouses/AddHouses";
 import MainPage from "./MainPage/MainPage";
+import Login from "./Login/Login";
 import {injector} from 'react-services-injector';
 import services from './Services';
 
@@ -16,6 +17,7 @@ const App = () =>
     <Router>
         <div>
             <Route exact path="/" component={MainPage}/>
+            <Route exact path="/login" component={Login}/>
             <Route exact path="/searchresult" component={SearchResult}/>
             <Route exact path="/credit" component={IncreaseCredit}/>
             <Route exact path="/addhouse" component={AddHouses}/>
