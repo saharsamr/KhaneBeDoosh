eb6ee999d423e6e98060872908b6ae3aeff93c51
SadafNiknam <sadaf.niknaam@gmail.com>
2018-05-19 15:50:43 +0430

    connecting to database.

--- src/main/phase_6/index.js
+++ src/main/phase_6/index.js
@@ -1,8 +1,35 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
+//
+// app.get("/", function (req, res) {
+//     res.send("Hello world!");
+// });
+//
+// var ex = app.listen(3000);
+const sqlite3 = require('sqlite3').verbose();
 
-app.get("/", function (req, res) {
-    res.send("Hello world!");
+// open database in memory
+let db = new sqlite3.Database('../data/KhaneBeDoosh.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the KBD SQlite database.');
 });
 
-var ex = app.listen(3000);
+db.serialize(() => {
+    db.each(`SELECT *
+           FROM  users`, (err, row) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log(row + "\t");
+    });
+});
+
+// close the database connection
+db.close((err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+});
